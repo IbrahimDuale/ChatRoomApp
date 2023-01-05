38f097de3a78011605b5c76a9415bb6d166a0143
Ibrahim Duale <ibrahimd@my.yorku.ca>
2023-01-04 21:47:54 -0500

    Added template and changed firebase key
    Added skeleton template of planned components and removed and changed firebase api key which was unintentionally added/

---
+++ app/src/components/Login/Login.jsx
@@ -0,0 +1,13 @@
+import "./Login.css";
+
+const Login = () => {
+
+
+    return (
+        <div className="login">
+            Login UI
+        </div>
+    )
+}
+
+export default Login;
---
+++ app/src/components/LoginController/LoginController.jsx
@@ -0,0 +1,13 @@
+import Login from "../Login/Login";
+import "./LoginController.css";
+
+const LoginController = () => {
+
+    return (
+        <div className="loginController">
+            <Login />
+        </div>
+    )
+}
+
+export default LoginController;
