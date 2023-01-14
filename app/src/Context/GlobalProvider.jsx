6befe0839fa9c7ef8a0af7ff6ccac5084d4299b3
Ibrahim Duale <ibrahimd@my.yorku.ca>
2023-01-13 19:37:01 -0500

    Implemented functionality for the login page
    Implemented and tested all functionality for the login page.

--- app/src/Context/GlobalProvider.jsx
+++ app/src/Context/GlobalProvider.jsx
@@ -1,12 +1,24 @@
 import { createContext } from "react"
-
+import { db } from "../firebase";
+import { doc, getDoc } from "firebase/firestore";
 
 const GlobalContext = createContext();
 
 const GlobalProvider = ({ children }) => {
+    /**
+     * Returns true if there is a room with ${room_id}.
+     * @param {string} room_id Id of the room
+     * @returns {Boolean} 
+     */
+    const room_exists = async (room_id) => {
+        const docRef = doc(db, "room_names", room_id);
+        const docSnap = await getDoc(docRef);
+
+        return docSnap.exists();
+    }
 
     return (
-        <GlobalContext.Provider value={{}}>
+        <GlobalContext.Provider value={{ room_exists }}>
             {children}
         </GlobalContext.Provider>
     )
--- app/src/firebase.js
+++ app/src/firebase.js
@@ -1,9 +1,12 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, connectAuthEmulator, } from "firebase/auth";
+import { getAuth, connectAuthEmulator, signInAnonymously } from "firebase/auth";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
-import { getDatabase, connectDatabaseEmulator } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBvtrVgpTe9uNtZLdGjGaHHOoeAx_U0b1c",
     authDomain: "chat-message-app-e1409.firebaseapp.com",
@@ -11,19 +14,28 @@ const firebaseConfig = {
     projectId: "chat-message-app-e1409",
     storageBucket: "chat-message-app-e1409.appspot.com",
     messagingSenderId: "105053602961",
-    appId: "1:105053602961:web:0e9dabe2141c1b72e8ab30"
+    appId: "1:105053602961:web:8eb3f71dabb66087e8ab30",
+    measurementId: "G-8VNKYC2V21"
 };
 
 
-
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore();
-const realtimeDb = getDatabase();
+signInAnonymously(auth)
+    .then(() => {
+        // Signed in..
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+    });
 
-connectFirestoreEmulator(db, 'localhost', 8082);
+const db = getFirestore(app);
+
+connectFirestoreEmulator(db, 'localhost', 8080);
 connectAuthEmulator(auth, "http://localhost:9099");
-connectDatabaseEmulator(realtimeDb, "localhost", 9000);
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export { db, auth, realtimeDb };
+export { db, auth };
